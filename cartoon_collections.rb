caaed58ae992981925254a0deb2667df9bf05925
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-05-01 13:47:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
 
-  planeteer_calls.map{|string|string.capitalize}
+  planeteer_calls.each{|string|string.capitalize}
   puts "#{string}!"
 
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
